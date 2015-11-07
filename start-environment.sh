4b98a622dde3a3775f584e02d14b56976144ce0f
Nicolas Mesa <nicolasmesa@gmail.com>
2015-11-07 11:43:40 -0500

    Adding install scripts

---
+++ start-environment.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+
+DIRPATH="/home/user01/spooler"
+USERGROUP="user01:user01"
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+
+echo "Setting up permissions"
+
+cp addqueue /bin/addqueue
+cp rmqueue /bin/rmqueue
+cp showqueue /bin/showqueue
+
+chown "$USERGROUP" /bin/addqueue /bin/rmqueue /bin/showqueue
+chmod 711 /bin/addqueue /bin/rmqueue /bin/showqueue
+chmod +s /bin/addqueue /bin/rmqueue /bin/showqueue
+
+echo "Creating directories"
+
+
+rm -rf "$DIRPATH"
+
+mkdir -p "$DIRPATH/config"
+mkdir -p "$DIRPATH/printer"
+
+echo "Setting directory permissions"
+
+chown -R "$USERGROUP" "$DIRPATH"
+
+chmod 700 -R "$DIRPATH"
+
+echo "Done"
