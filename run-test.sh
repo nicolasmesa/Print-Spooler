33655c82621ff34348518908c0a65bdd0ac09ada
Nicolas Mesa <nicolasmesa@gmail.com>
2015-11-10 14:01:57 -0500

    Changing tests script

--- run-test.sh
+++ run-test.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
+echo "
+---------------------------------
+|	Tests started		|
+---------------------------------
+
+"
+
 addqueue test_data/*
 showqueue
 rmqueue 1 2 3 4 5 6 7 8
@@ -7,3 +14,10 @@ rmqueue 1 2 3 4 5 6 7 8
 addqueue test_data/file01
 showqueue
 rmqueue 1 2 3 4 5 6 7 8
+
+echo "
+---------------------------------
+|       Tests ended		|
+---------------------------------
+
+"
